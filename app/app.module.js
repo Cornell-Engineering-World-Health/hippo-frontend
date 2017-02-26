a2c3d26c8c2561d8d83e4ceb18dabf432577db42
ec587 <ec587@cornell.edu>
2017-02-26 01:56:31 -0500

    Applied Standard style fixes.

--- app/app.module.js
+++ app/app.module.js
@@ -1,14 +1,14 @@
-﻿/*
+/*
 
 APP MODULE handle the setup of app, load in angular dependencies, etc.
 
 */
 
 (function () {
-    'use strict';
+  'use strict'
 
-    angular.module('app', [
-        'app.core.services',
-        'app.core.directives'
-    ]);
-})();
+  angular.module('app', [
+    'app.core.services',
+    'app.core.directives'
+  ])
+})()
