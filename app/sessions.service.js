74f7185b77000a74485e9a5a0bd651db7a189109
Ben Edwards <bje43@cornell.edu>
2017-03-01 12:25:37 -0500

    added function for deleting sessions and getting new tokens

--- app/sessions.service.js
+++ app/sessions.service.js
@@ -5,19 +5,35 @@
         'app.core.directives'
     ]);
     
-    create.$inject ['$http']
-    function create ($http) {
+    /*given a caller_id and callee_id creates a openTok Session accesible to both users*/
+    createSession.$inject ['$http']
+    function createSession ($http, caller_id, callee_id) {
       var service =  {
-        return $http.post(/videos)
+        return $http.post(/videos/caller_id/callee_id)
           .then(function (response) {
-            sessions.push (response.data)
             return response.data  
           })
       }
     }
-   
-    get.$inject ['$http']
-    function get ($http) {
-
+    /*given a session_id generates a new token for that session*/
+    getNewToken.$inject ['$http']
+    function getNewToken ($http, session_id) {
+     var service =  {
+        return $http.get(/videos/session_id)
+          .then(function (response) {
+            return response.data  
+          })
+      }
     }
+    /*deletes session with given session_name*/
+    deleteSession.$inject ['$http']
+    function deleteSession ($http, session_name){
+      var service = {
+        return $http.delete(/videos/session_name)
+        .then( function (response{
+          return response.data
+        })
+      }
+    } 
+   
 }) ();
