3d571f693baee9db0318526cd095cc5fc2927436
Lillyan <ldp54@cornell.edu>
2017-03-03 16:32:10 -0500

    adding disconnect to scope

--- app/shared/components/video/videoController.js
+++ app/shared/components/video/videoController.js
@@ -9,58 +9,71 @@
 
     function VideoController($scope, $log, sessionFactory, opentok){
 
-    // Create the session
-    $scope.newSession = function(sessionData) {
-        var resp = createSession(sessionData) //error handling
-        $scope.sessionID = resp.sessionID
-        $scope.session = opentok.initSession($scope.sessionID)
-    }
+        // Create the session
+        $scope.newSession = function(sessionData) {
+            var resp = createSession(sessionData) //error handling
+            $scope.sessionID = resp.sessionID
+            $scope.session = opentok.initSession($scope.sessionID)
+        }
 
 
-    // Connect to session
-    $scope.connectToSession = function() {
-        $scope.token = getNewToken($scope.sessionID)
-        $scope.session.connect($scope.token, function(error) {
-            if (error) {
-                console.log('[ERROR] Connect to session error', error)
-            }
-            else {
-                console.log('Connected to session', $scope.sessionID)
-            }
+        // Connect to session
+        $scope.connectToSession = function() {
+            $scope.token = getNewToken($scope.sessionID)
+            $scope.session.connect($scope.token, function(error) {
+                if (error) {
+                    console.log('[ERROR] Connect to session error', error)
+                }
+                else {
+                    console.log('Connected to session', $scope.sessionID)
+                }
+            })
+        }
+
+        // Create publisher
+        $scope.createPublisher = function() {
+            // hard-coded properites for now
+            var targetElement = 'publisher'
+            var properties = {  insertMode: 'append',
+                                width: '100%',
+                                height: '100%' }
+            return opentok.initPublisher(targetElement, properties, function(error){
+                if(error) {
+                    console.log('[ERROR] Initializing publisher error', error)
+                } else {
+                    console.log('Initialized publisher...')
+                }
+            })
+        }
+
+        $scope.publisher = createPublisher()
+
+        // Publish
+        $scope.publishToSession = function() {
+            $scope.session.publish($scope.publisher, function() {
+                if(error) {
+                    console.log('[ERROR] Publishing to session error', error)
+                }
+                else {
+                    console.log('Published to stream...')
+                }
+            })
+        }
+
+        $scope.endSession = function() {
+            $scope.session.disconnect()
+            $scope.session.on("sessionDisconnected", function (event) {
+              // The event is defined by the SessionDisconnectEvent class
+              if (event.reason == "networkDisconnected") {
+                alert("Your network connection terminated.")
+              }
+              // LOGIC TO CHANGE VIEW GOES HERE
+              // Can also add connectionDestroyed
+            });
+            sessionFactory.delete(session_name)
+              .success(function(data) {
+                $scope.res = data;
+              });
         }
     }
-
-    // Create publisher
-    $scope.createPublisher = function() {
-        // hard-coded properites for now
-        var targetElement = 'publisher'
-        var properties = {  insertMode: 'append',
-                            width: '100%',
-                            height: '100%' }
-        return opentok.initPublisher(targetElement, properties, function(error){
-            if(error) {
-                console.log('[ERROR] Initializing publisher error', error)
-            } else {
-                console.log('Initialized publisher...')
-            }
-        })
-    }
-
-    $scope.publisher = createPublisher()
-
-    // Publish
-    $scope.publishToSession= function() {
-        $scope.session.publish($scope.publisher, function() {
-            if(error) {
-                console.log('[ERROR] Publishing to session error', error)
-            }
-            else {
-                console.log('Published to stream...')
-            }
-        }
-    })
-      
-      
-    }
-])
-}
+})()
