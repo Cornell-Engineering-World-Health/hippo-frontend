d9f25653705dfe666cace3e4edda64bfed43b543
Lillyan <ldp54@cornell.edu>
2017-03-03 17:37:21 -0500

    angular frontend

--- app/app.module.js
+++ app/app.module.js
@@ -7,11 +7,18 @@ APP MODULE handle the setup of app, load in angular dependencies, etc.
 (function () {
     'use strict';
 
-    angular.module('app', [
+    var app = angular.module('app', [
         'app.core.services',
         'app.core.directives',
         'ngRoute',
         'app.video',
         'opentokjs'
     ]);
+    app.config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: "/index.html",
+          controller: "VideoController as vc" //change the name of the controller
+        })
+    })
 })();
