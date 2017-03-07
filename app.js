55ce7c0777e7f6be81676f4b347b4e56c31a219b
Ben Edwards <bje43@cornell.edu>
2017-03-07 00:40:54 -0500

    fixed problems with service

--- app.js
+++ app.js
@@ -9,174 +9,185 @@
 //       token: 'YOUR_TOKEN'
 //   });
 
+var app = angular.module('myApp', ['opentok', 'ngRoute'])
 
-var app = angular.module( "myApp", ['opentok', 'ngRoute'] );
-
-app.config( function ( $routeProvider ) {
+app.config(function ($routeProvider) {
   $routeProvider
-    .when( '/video', { templateUrl: 'index.html' } )
-    .when( '/', { templateUrl: 'index.html' } )
-    .when( '/home', { templateUrl: 'index.html' } )
-    .when( '/videocall', { templateUrl: 'video.html' } )
+    .when('/video', { templateUrl: 'index.html' })
+    .when('/', { templateUrl: 'index.html' })
+    .when('/home', { templateUrl: 'index.html' })
+    .when('/videocall', { templateUrl: 'video.html' })
     // .otherwise( { redirectTo: '/video' } );
-});
-app.factory('sessionFactory', ['$http', function($http) {
+})
+app.factory('sessionFactory', ['$http', function ($http) {
+  var baseURL = 'https://ewh-hippo.herokuapp.com/api'
   var service = {
-    createSession: function(sessionData) {
+    createSession: function (sessionData) {
       // TODO: why are we exposing this in post url; no need
-      return $http.post('/videos/' + sessionData.caller_id + '/users/' + sessionData.callee_id, sessionData)
-        .then(function(response) {
-          return alert("success");
-        })
-        .catch(function(error) {
-          console.error('Failed post. Error: ' + error.data);
+      return $http.post(baseURL + '/videos/' + sessionData.caller_id + '/users/' + sessionData.calling_id, sessionData)
+         .then(function (response) {
+           return response.data
+         })
+        .catch(function (error) {
+          console.error('Failed post. Error: ' + error.data)
         })
     },
-    getNewToken: function(session_name) {
-      return $http.get('/videos/' + session_name)
+    getNewToken: function (session_name) {
+      return $http.get(baseURL + '/videos/' + session_name)
         .then(function (response) {
           return response.data
         })
-        .catch(function(error) {
-          console.error('Failed to get token. Error: ' + error.data);
+        .catch(function (error) {
+          console.error('Failed to get token. Error: ' + error.data)
         })
     },
-    // TODO: why are we not using session_name.
-    deleteSession: function(session_name) {
-      return $http.delete('/videos/' + session_name)
-        .then( function (response){
+    deleteSession: function (session_name) {
+      return $http.delete(baseURL + '/videos/' + session_name)
+        .then(function (response) {
           return response.data
         })
-        .catch(function(error) {
-          console.error('Failed to delete session. Error: ' + error.data);
+        .catch(function (error) {
+          console.error('Failed to delete session. Error: ' + error.data)
         })
     }
-  };
-  return service;
+  }
+  return service
 }])
 
-app.controller('UserController', ['$scope', '$log', 'sessionFactory', function($scope, $log, sessionFactory) {
+app.controller('UserController', ['$scope', '$log', 'sessionFactory', function ($scope, $log, sessionFactory) {
   $scope.users =
   [
     {
-      "userId": 0,
-      "firstName": "Ann",
-      "lastName": "Brown",
-      "email": "ab@gmail.com",
+      'userId': 0,
+      'firstName': 'Ann',
+      'lastName': 'Brown',
+      'email': 'ab@gmail.com'
     }
   ]
   $scope.userSessions =
   [
     {
-      datetime: "2017-03-05T14:24:17.725Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg1NzY5OH5WR0c4UEMwTTJ2SlQ5akNDbENWMUhXS1h-UH4",
-      name: "okapi453",
-      _id: "58bc1f91eaad2d00119618f8",
+      datetime: '2017-03-05T14:24:17.725Z',
+      sessionId: '1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg1NzY5OH5WR0c4UEMwTTJ2SlQ5akNDbENWMUhXS1h-UH4',
+      name: 'okapi453',
+      _id: '58bc1f91eaad2d00119618f8',
       participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9ZGYxNmJmZGI4ZDBlZmIzZDNkY2IwNjIyZDljOTRmNzIzMjNmMzVhNDpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnMU56WTVPSDVXUjBjNFVFTXdUVEoyU2xRNWFrTkRiRU5XTVVoWFMxaC1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg1OCZub25jZT0wLjM3OTk5Njg0NDQzMTYwNDQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ4ODgxMDI1OA=="
+      tokenId: 'T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9ZGYxNmJmZGI4ZDBlZmIzZDNkY2IwNjIyZDljOTRmNzIzMjNmMzVhNDpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnMU56WTVPSDVXUjBjNFVFTXdUVEoyU2xRNWFrTkRiRU5XTVVoWFMxaC1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg1OCZub25jZT0wLjM3OTk5Njg0NDQzMTYwNDQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ4ODgxMDI1OA=='
     },
     {
-      datetime: "2017-03-05T14:29:44.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "dinosaur392",
-      _id: "58bc1faceaad2d00119618f9",
+      datetime: '2017-03-05T14:29:44.805Z',
+      sessionId: '1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4',
+      name: 'dinosaur392',
+      _id: '58bc1faceaad2d00119618f9',
       participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
+      tokenId: 'T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1'
     },
     {
-      datetime: "2017-02-02T10:24:18.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "turtle406",
-      _id: "58bc1faceaad2d00119618f9",
+      datetime: '2017-02-02T10:24:18.805Z',
+      sessionId: '1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4',
+      name: 'turtle406',
+      _id: '58bc1faceaad2d00119618f9',
       participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
+      tokenId: 'T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1'
     },
     {
-      datetime: "2017-06-25T18:24:03.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "crocodile142",
-      _id: "58bc1faceaad2d00119618f9",
+      datetime: '2017-06-25T18:24:03.805Z',
+      sessionId: '1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4',
+      name: 'crocodile142',
+      _id: '58bc1faceaad2d00119618f9',
       participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
+      tokenId: 'T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1'
     },
     {
-      datetime: "2017-02-20T12:25:22.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "weasel821",
-      _id: "58bc1faceaad2d00119618f9",
+      datetime: '2017-02-20T12:25:22.805Z',
+      sessionId: '1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4',
+      name: 'weasel821',
+      _id: '58bc1faceaad2d00119618f9',
       participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
+      tokenId: 'T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1'
     }
   ]
 }])
 
 // app.controller('VideoController', ['$scope', '$log','sessionFactory','OTSession', function($scope, $log, sessionFactory, OTSession) {
-app.controller('VideoController', ['$scope', '$http', '$window', '$log', 'OTSession', function($scope, $http, $window, $log, OTSession, sessionFactory) {
+app.controller('VideoController', ['$scope', '$http', '$window', '$log', 'OTSession', 'sessionFactory', function ($scope, $http, $window, $log, OTSession, sessionFactory) {
   console.log(OTSession)
-  $scope.streams = OTSession.streams;
-  $scope.connections = OTSession.connections;
-  $scope.publishing = false;
-  $scope.archiveId = null;
-  $scope.archiving = false;
-  $scope.connected = false;
-  $scope.reconnecting = false;
-  $scope.leaving = false;
-  $scope.getVideo = function() {
+  $scope.streams = OTSession.streams
+  $scope.connections = OTSession.connections
+  $scope.publishing = false
+  $scope.archiveId = null
+  $scope.archiving = false
+  $scope.connected = false
+  $scope.reconnecting = false
+  $scope.leaving = false
+
+  $scope.getVideo = function () {
     if ($scope.session) {
-      $scope.session.disconnect();
+      $scope.session.disconnect()
     }
-    OTSession.init('45786882', '1_MX40NTc4Njg4Mn5-MTQ4ODY1ODQ4MDEwMn5SZW5Rd1RlTlpKNGkzcWtBaEFpYjNMNmd-fg', "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9ODRhOTMyZmM2YjI4MWJkOTBiODAwMDliOGNlOTg5M2NiYTBkZWI3NzpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRFkxT0RRNE1ERXdNbjVTWlc1UmQxUmxUbHBLTkdremNXdEJhRUZwWWpOTU5tZC1mZyZjcmVhdGVfdGltZT0xNDg4NjU4NDk0Jm5vbmNlPTAuNTU5OTQzMzk5Njg5MzcxMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDkxMjQ2ODkz", function(err, session) {
+    OTSession.init('45786882', '1_MX40NTc4Njg4Mn5-MTQ4ODY1ODQ4MDEwMn5SZW5Rd1RlTlpKNGkzcWtBaEFpYjNMNmd-fg', 'T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9ODRhOTMyZmM2YjI4MWJkOTBiODAwMDliOGNlOTg5M2NiYTBkZWI3NzpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRFkxT0RRNE1ERXdNbjVTWlc1UmQxUmxUbHBLTkdremNXdEJhRUZwWWpOTU5tZC1mZyZjcmVhdGVfdGltZT0xNDg4NjU4NDk0Jm5vbmNlPTAuNTU5OTQzMzk5Njg5MzcxMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDkxMjQ2ODkz', function (err, session) {
+     /* var createJson = {'caller_id': 8, 'calling_id': 9}
+      var createPromise = sessionFactory.createSession(createJson).then(function (result) {
+        console.log(result)
+        var newTokenPromise = sessionFactory.getNewToken(result.data.name).then(function (result_token) {
+          console.log(result_token)
+        })
+        var deletePromise = sessionFactory.deleteSession(result.data.name).then(function (result_delete) {
+          console.log(result_delete)
+        })
+      }) */
+
       if (err) {
-        $scope.$broadcast('otError', {message: 'initialize session error'});
-        return;
+        $scope.$broadcast('otError', {message: 'initialize session error'})
+        return
       }
-      $scope.session = session;
-      var connectDisconnect = function(connected) {
-            $scope.$apply(function() {
-              $scope.connected = connected;
-              $scope.reconnecting = false;
-              if (!connected) {
-                $scope.publishing = false;
-              }
-            });
+      $scope.session = session
+      var connectDisconnect = function (connected) {
+        $scope.$apply(function () {
+          $scope.connected = connected
+          $scope.reconnecting = false
+          if (!connected) {
+            $scope.publishing = false
+          }
+        })
       }
-      var reconnecting = function(isReconnecting) {
-        $scope.$apply(function() {
-          $scope.reconnecting = isReconnecting;
-        });
-      };
+      var reconnecting = function (isReconnecting) {
+        $scope.$apply(function () {
+          $scope.reconnecting = isReconnecting
+        })
+      }
       if ((session.is && session.is('connected')) || session.connected) {
-        connectDisconnect(true);
+        connectDisconnect(true)
       }
-      $scope.session.on('sessionConnected', connectDisconnect.bind($scope.session, true));
-      $scope.session.on('sessionDisconnected', connectDisconnect.bind($scope.session, false));
-      $scope.session.on('archiveStarted archiveStopped', function(event) {
+      $scope.session.on('sessionConnected', connectDisconnect.bind($scope.session, true))
+      $scope.session.on('sessionDisconnected', connectDisconnect.bind($scope.session, false))
+      $scope.session.on('archiveStarted archiveStopped', function (event) {
         // event.id is the archiveId
-        $scope.$apply(function() {
-          $scope.archiveId = event.id;
-          $scope.archiving = (event.type === 'archiveStarted');
-        });
-      });
-      $scope.session.on('sessionReconnecting', reconnecting.bind($scope.session, true));
-      $scope.session.on('sessionReconnected', reconnecting.bind($scope.session, false));
-    });
-    $scope.publishing = true;
+        $scope.$apply(function () {
+          $scope.archiveId = event.id
+          $scope.archiving = (event.type === 'archiveStarted')
+        })
+      })
+      $scope.session.on('sessionReconnecting', reconnecting.bind($scope.session, true))
+      $scope.session.on('sessionReconnected', reconnecting.bind($scope.session, false))
+    })
+    $scope.publishing = true
   }
-  $scope.$on('$destroy', function() {
+  $scope.$on('$destroy', function () {
     if ($scope.session && $scope.connected) {
-      $scope.session.disconnect();
-      $scope.connected = false;
+      $scope.session.disconnect()
+      $scope.connected = false
     }
-    $scope.session = null;
-  });
-  $scope.endVideo = function() {
-    console.log("in ending Video")
+    $scope.session = null
+  })
+  $scope.endVideo = function () {
+    console.log('in ending Video')
     if (!$scope.leaving) {
-      $scope.leaving = true;
-      $scope.session.disconnect();
-      $scope.session.on('sessionDisconnected', function() {
+      $scope.leaving = true
+      $scope.session.disconnect()
+      $scope.session.on('sessionDisconnected', function () {
         console.log('Session disconnected.')
-      });
+      })
     }
-  };
+  }
 }])
