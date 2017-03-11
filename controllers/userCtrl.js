8eb3687946257fa248c8920c5f30cdb9ab9bb302
ec587 <ec587@cornell.edu>
2017-03-11 01:36:31 -0500

    Switched out hard-coded demo ctrls for draft of actual ones.

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,54 +1,30 @@
-app.controller('UserCtrl', ['$scope', '$log', function($scope, $log) {
-  $scope.users =
-  [
-    {
-      "userId": 0,
-      "firstName": "Ann",
-      "lastName": "Brown",
-      "email": "ab@gmail.com",
-    }
-  ]
-  $scope.userSessions =
-  [
-    {
-      datetime: "2017-03-05T14:24:17.725Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg1NzY5OH5WR0c4UEMwTTJ2SlQ5akNDbENWMUhXS1h-UH4",
-      name: "okapi453",
-      _id: "58bc1f91eaad2d00119618f8",
-      participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9ZGYxNmJmZGI4ZDBlZmIzZDNkY2IwNjIyZDljOTRmNzIzMjNmMzVhNDpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnMU56WTVPSDVXUjBjNFVFTXdUVEoyU2xRNWFrTkRiRU5XTVVoWFMxaC1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg1OCZub25jZT0wLjM3OTk5Njg0NDQzMTYwNDQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ4ODgxMDI1OA=="
-    },
-    {
-      datetime: "2017-03-05T14:29:44.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "dinosaur392",
-      _id: "58bc1faceaad2d00119618f9",
-      participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
-    },
-    {
-      datetime: "2017-02-02T10:24:18.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "turtle406",
-      _id: "58bc1faceaad2d00119618f9",
-      participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
-    },
-    {
-      datetime: "2017-06-25T18:24:03.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "crocodile142",
-      _id: "58bc1faceaad2d00119618f9",
-      participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
-    },
-    {
-      datetime: "2017-02-20T12:25:22.805Z",
-      sessionId: "1_MX40NTc4Njg4Mn5-MTQ4ODcyMzg4NDc5Mn5XZ2UzUjd4Uk9oSUxaeThQdVNLVUhWblN-UH4",
-      name: "weasel821",
-      _id: "58bc1faceaad2d00119618f9",
-      participants: [],
-      tokenId: "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9NGE0MDczY2E4MWIzY2EyODk4ZDIyYWM3ZTZlOTBjOGE3ZDc1NzQ3ZTpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRGN5TXpnNE5EYzVNbjVYWjJVelVqZDRVazlvU1V4YWVUaFFkVk5MVlVoV2JsTi1VSDQmY3JlYXRlX3RpbWU9MTQ4ODcyMzg4NSZub25jZT0wLjc3NjQwNDA5OTkwNDE0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg4ODEwMjg1"
-    }
-  ]
+app.controller('UserCtrl', ['$scope', '$log', '$http', function ($scope, $log, $http) {
+
+  var baseURL = 'https://ewh-hippo.herokuapp.com/api'
+
+  // Returns user information for the User profile
+  $scope.getUser = function(user_id) {
+    $http.get(baseURL + '/users/' + user_id)
+        .then(function (response) {
+            return response.data
+        })
+        .catch(function (error) {
+            console.log('Failed to get user ' + user_id + '. Error: ' + error.data)
+            return error
+        })
+  }
+  // Returns all available sessions given user_id
+  $scope.getAllSessionsForUser = function(user_id) {
+      $http.get(baseURL + '/users/' + user_id)
+        .then(function (response) {
+            console.log(response.data.calls)
+            return response.data.calls
+        })
+        .catch(function (error) {
+            console.log('Failed to get sessions for user ' + user_id + '. Error: ' + error.data)
+            return error.data
+        })
+  }
+  // Hard coded for user 8 
+  $scope.userSessions = $scope.getAllSessionsForUser(8)
 }])
--- controllers/videoCtrl.js
+++ controllers/videoCtrl.js
@@ -1,69 +1,92 @@
-app.controller('VideoCtrl', ['$scope', '$http', '$window', '$log', 'OTSession', function($scope, $http, $window, $log, OTSession) {
-  console.log(OTSession)
-  $scope.streams = OTSession.streams;
-  $scope.connections = OTSession.connections;
-  $scope.publishing = false;
-  $scope.archiveId = null;
-  $scope.archiving = false;
-  $scope.connected = false;
-  $scope.reconnecting = false;
-  $scope.leaving = false;
-  $scope.getVideo = function() {
-    if ($scope.session) {
-      $scope.session.disconnect();
-    }
-    OTSession.init('45786882', '1_MX40NTc4Njg4Mn5-MTQ4ODY1ODQ4MDEwMn5SZW5Rd1RlTlpKNGkzcWtBaEFpYjNMNmd-fg', "T1==cGFydG5lcl9pZD00NTc4Njg4MiZzaWc9ODRhOTMyZmM2YjI4MWJkOTBiODAwMDliOGNlOTg5M2NiYTBkZWI3NzpzZXNzaW9uX2lkPTFfTVg0ME5UYzROamc0TW41LU1UUTRPRFkxT0RRNE1ERXdNbjVTWlc1UmQxUmxUbHBLTkdremNXdEJhRUZwWWpOTU5tZC1mZyZjcmVhdGVfdGltZT0xNDg4NjU4NDk0Jm5vbmNlPTAuNTU5OTQzMzk5Njg5MzcxMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDkxMjQ2ODkz", function(err, session) {
-      if (err) {
-        $scope.$broadcast('otError', {message: 'initialize session error'});
-        return;
-      }
-      $scope.session = session;
-      var connectDisconnect = function(connected) {
-            $scope.$apply(function() {
-              $scope.connected = connected;
-              $scope.reconnecting = false;
-              if (!connected) {
-                $scope.publishing = false;
-              }
-            });
-      }
-      var reconnecting = function(isReconnecting) {
-        $scope.$apply(function() {
-          $scope.reconnecting = isReconnecting;
-        });
-      };
-      if ((session.is && session.is('connected')) || session.connected) {
-        connectDisconnect(true);
-      }
-      $scope.session.on('sessionConnected', connectDisconnect.bind($scope.session, true));
-      $scope.session.on('sessionDisconnected', connectDisconnect.bind($scope.session, false));
-      $scope.session.on('archiveStarted archiveStopped', function(event) {
-        // event.id is the archiveId
-        $scope.$apply(function() {
-          $scope.archiveId = event.id;
-          $scope.archiving = (event.type === 'archiveStarted');
-        });
-      });
-      $scope.session.on('sessionReconnecting', reconnecting.bind($scope.session, true));
-      $scope.session.on('sessionReconnected', reconnecting.bind($scope.session, false));
-    });
-    $scope.publishing = true;
-  }
-  $scope.$on('$destroy', function() {
-    if ($scope.session && $scope.connected) {
-      $scope.session.disconnect();
-      $scope.connected = false;
-    }
-    $scope.session = null;
+app.controller('VideoController', ['$scope', '$http', '$window', '$log', 'OTSession', 'VideoService', 
+  function ($scope, $http, $window, $log, OTSession, VideoService) {
+  
+  $scope.streams = OTSession.streams
+  $scope.connections = OTSession.connections
+  $scope.publishing = false
+  $scope.archiveId = null
+  $scope.archiving = false
+  $scope.connected = false
+  $scope.reconnecting = false
+  $scope.leaving = false
+  $scope.deleted = false
+
+  // Wrap in VideoService?
+  $http.get('./config.json').success(function(data) {
+    console.log(data)
+    $scope.apiKey = data.apiKey;
   });
-  $scope.endVideo = function() {
-    console.log("in ending Video")
-    if (!$scope.leaving) {
-      $scope.leaving = true;
-      $scope.session.disconnect();
-      $scope.session.on('sessionDisconnected', function() {
-        console.log('Session disconnected.')
-      });
+
+  $scope.getVideoByName = function (session_name) {
+    if ($scope.session) {
+      $scope.session.disconnect()
     }
-  };
+
+    VideoService.getNewToken(session_name)
+      .then(function (result_token) {
+        console.log(result_token)
+
+        OTSession.init($scope.apiKey, result_token.sessionId, result_token.tokenId, function(err, session) {
+          if(err) {
+            console.log('sessionId: ' + result_token.sessionId + ' tokenId: ' + result_token.tokenId)
+            $scope.$broadcast('otError', {message: 'initialize session error'})
+            return
+          }
+          
+          $scope.session = session
+          $scope.sessionName = session_name
+
+          var connectDisconnect = function (connected) {
+            $scope.$apply(function () {
+              $scope.connected = connected
+              $scope.reconnecting = false
+              if (!connected) {
+                $scope.publishing = false
+              }
+            })
+          }
+
+          var reconnecting = function (isReconnecting) {
+            $scope.$apply(function () {
+              $scope.reconnecting = isReconnecting
+            })
+          }
+
+          if ((session.is && session.is('connected')) || session.connected) {
+            connectDisconnect(true)
+          }
+
+          $scope.session.on('sessionConnected', connectDisconnect.bind($scope.session, true))
+          $scope.session.on('sessionDisconnected', connectDisconnect.bind($scope.session, false))
+
+          $scope.session.on('sessionReconnecting', reconnecting.bind($scope.session, true))
+          $scope.session.on('sessionReconnected', reconnecting.bind($scope.session, false))
+        })
+
+        $scope.publishing = true
+        return result_token
+      })   
+  }
+
+  $scope.$on('$destroy', function () {
+    if ($scope.session && $scope.connected) {
+      $scope.session.disconnect()
+      $scope.connected = false
+    }
+    $scope.session = null
+  })
+
+  $scope.endVideo = function () {
+    console.log('in ending Video')
+    if (!$scope.leaving) {
+      $scope.leaving = true
+      $scope.session.disconnect() //Disconnects and unpublishes
+
+      $scope.session.on('sessionDisconnected', function () {
+        console.log('Session disconnected.')
+
+      VideoService.deleteSession($scope.sessionName)
+      })
+    }
+  }
 }])
