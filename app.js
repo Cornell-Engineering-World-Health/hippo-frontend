72b131f65d46972a3fa36bf3ac95b0ac63798906
ec587 <ec587@cornell.edu>
2017-03-15 13:16:21 -0400

    Removed OAth

--- app.js
+++ app.js
@@ -10,70 +10,25 @@
 //   });
 
 
-var app = angular.module( "myApp", ['opentok', 'ui.router', 'satellizer'] );
+var app = angular.module( "myApp", ['opentok', 'ui.router'] );
 
-app.config( function ( $stateProvider, $urlRouterProvider, $authProvider ) {
-  var skipIfLoggedIn = ['$q', '$auth', function($q, $auth) {
-      var deferred = $q.defer();
-      if ($auth.isAuthenticated()) {
-        deferred.reject();
-      } else {
-        deferred.resolve();
-      }
-      return deferred.promise;
-    }]
-
-    var loginRequired = ['$q', '$location', '$auth', function($q, $location, $auth) {
-      var deferred = $q.defer();
-      if ($auth.isAuthenticated()) {
-        deferred.resolve();
-      } else {
-        $location.path('/login');
-      }
-      return deferred.promise;
-    }]
-
+app.config( function ( $stateProvider, $urlRouterProvider ) {
   $stateProvider
-    .state('login', {
-      url: '/login',
-      templateUrl: 'partials/login.html',
-      controller: 'LoginCtrl',
-      resolve: {
-        skipIfLoggedIn: skipIfLoggedIn
-      }
-    })
-    .state('home', {
-      url: '/',
-      templateUrl: 'index.html',
-      controller: 'LoginCtrl',
-      resolve: {
-        skipIfLoggedIn: skipIfLoggedIn
-      }
-    })
-    .state('logout', {
-      url: '/logout',
-      template: null,
-      controller: 'LogoutCtrl'
-    })
     .state('videolist', {
       url: '/user',
       templateUrl: 'partials/videolist.html',
       controller: 'UserCtrl'
-    }) 
+    })
     .state('video', {
       url: '/videocall',
       templateUrl: 'partials/video.html',
       controller: 'VideoCtrl'
     })
-   
+
     $urlRouterProvider.otherwise('/');
     // .when( '/video', { templateUrl: 'index.html' } )
     // .when( '/', { templateUrl: 'index.html' } )
     // .when( '/home', { templateUrl: 'index.html' } )
     // .when( '/videocall', { templateUrl: 'video.html' } )
     // .otherwise( { redirectTo: '/video' } );
-
-    $authProvider.google({
-      clientId: '711259324524-36d8oudk6g40g1ioel59hhegcjd3n5t1.apps.googleusercontent.com'
-    });
 });
