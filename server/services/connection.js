5181b30c478b7f085d69925f55533530dae46ed8
ec587 <ec587@cornell.edu>
2017-03-16 13:38:16 -0400

    New server.js file with package.json dependencies updated and server backend removed.

--- server/services/connection.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-var mongoose = require('mongoose')
-var autoIncrement = require('mongoose-auto-increment')
-var connection = mongoose.createConnection(process.env.DB_MONGODB_URL)
-
-// initialize mongoose-auto-increment with our mongo connection
-autoIncrement.initialize(connection)
-
-module.exports = connection
