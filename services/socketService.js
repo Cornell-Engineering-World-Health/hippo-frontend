7245a46441c562de6141466dbd6dba0200bb3af2
Ben Edwards <bje43@cornell.edu>
2017-03-24 19:57:41 -0400

    fixed socketService integrated with backend

--- services/socketService.js
+++ services/socketService.js
@@ -1,14 +1,18 @@
-app.factory('SocketService', ['$rootScope', function($rootScope) {
-  $rootScope.socket = io.connect();
-  console.log($rootScope.socket);
-
+app.factory('SocketService', ['$rootScope', function ($rootScope) {
+  // $rootScope.socket = io.connect('http://localhost:3000', {'force new connection': true})
+  if ($rootScope.socket == null) {
+    console.log('just before socket creation')
+    $rootScope.socket = io.connect('10.132.2.122:3000', { transports: ['websocket', 'polling', 'flashsocket'] })
+    $rootScope.socket.emit('user-online', {email: 'bje43@cornell.edu' })
+    console.log($rootScope.socket)
+  }
   return {
-    on: function(eventName, callback){
-      $rootScope.socket.on(eventName, callback);
+    on: function (eventName, callback) {
+      $rootScope.socket.on(eventName, callback)
     },
-    emit: function(eventName, data) {
-      $rootScope.socket.emit(eventName, data);
+    emit: function (eventName, data) {
+      $rootScope.socket.emit(eventName, data)
     }
-  };
-}]);
+  }
+}])
 
