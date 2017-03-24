6a4c760cc8ac03201bc44264b3fc916a5ad43ba0
Lillyan <ldp54@cornell.edu>
2017-03-24 18:28:28 -0400

    desperate

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-// npm start
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8080);
-console.log("Server listening on: http://localhost:%s", 8080);
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static(__dirname + '/app'));
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
