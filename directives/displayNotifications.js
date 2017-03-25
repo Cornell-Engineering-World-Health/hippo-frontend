18a80c93e15ec84042c2c66647a63eb436f647f5
Ben Edwards <bje43@cornell.edu>
2017-03-25 16:27:56 -0400

    made notifications closeable

--- directives/displayNotifications.js
+++ directives/displayNotifications.js
@@ -9,7 +9,7 @@ app.directive('displayNotifications', [function (socketService) {
         for (var i = 0; i < scope.notifications.length; i++) {
          // element.append('<p class = "notification">' + scope.notifications[i] + '</p>')
           // ngNotify.set(scope.notifications[i])
-          element.append('<div class="alert alert-info alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>' + scope.notifications[i] + '</div>')
+          element.append('<div class="alert alert-warning alert-dismissible" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + scope.notifications[i] + '</div>')
           console.log(scope.notifications[i])
         }
         // empty notificatiosn
