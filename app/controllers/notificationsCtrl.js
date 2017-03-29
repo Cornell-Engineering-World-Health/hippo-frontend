0535003155c75cd04fd24da19ae07c169416ec16
ec587 <ec587@cornell.edu>
2017-03-29 18:24:18 -0400

    Notifications

---
+++ app/controllers/notificationsCtrl.js
@@ -0,0 +1,14 @@
+app.controller('NotificationsCtrl', ['$scope', '$log', 'SocketService', function ($scope, $log, SocketService) {
+  $scope.notifications = ['You are the first to join the session!']
+  //console.log($scope.notifications)
+  SocketService.on('user-has-connected', function (data) {
+    $scope.notifications.push(data.joiner +' has connected')
+  })
+  SocketService.on('participant-disconnected', function (disconnectInfo) {
+    $scope.notifications.push('A participant has disconnected')
+  })
+  SocketService.on('confirmation', function (data) {
+    //console.log(data.msg)
+    $scope.notifications.push('confirmation has been recieved')
+  })
+}])
---
+++ app/services/socketService.js
@@ -0,0 +1,25 @@
+app.factory('SocketService', ['$rootScope', '$http', function ($rootScope, $http) {
+  // $rootScope.socket = io.connect('http://localhost:3000', {'force new connection': true})
+  if ($rootScope.socket == null) {
+
+    //console.log('just before socket creation')
+    $rootScope.socket = io.connect('https://ewh-hippo.herokuapp.com/', { transports: ['websocket', 'polling', 'flashsocket'] })
+
+    var userEmail = ""
+    $http.get('https://ewh-hippo.herokuapp.com/api/self').success(function (data) {
+      userEmail = data.email
+      $rootScope.socket.emit('user-online', {email: userEmail})
+    })
+
+    //console.log($rootScope.socket)
+  }
+
+  return {
+    on: function (eventName, callback) {
+      $rootScope.socket.on(eventName, callback)
+    },
+    emit: function (eventName, data) {
+      $rootScope.socket.emit(eventName, data)
+    }
+  }
+}])
