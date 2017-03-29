1e9d7bcfd841c021e43a812a6715c66e38aa7695
Ben Edwards <bje43@cornell.edu>
2017-03-29 19:17:02 -0400

    apply angular DOM changes

--- app/directives/displayNotifications.js
+++ app/directives/displayNotifications.js
@@ -3,15 +3,16 @@ app.directive('displayNotifications', [function (socketService) {
     restrict: 'EA', // directive can only be used as an element or attribute
     link: function (scope, element, attrs) {
       scope.$watch('notifications', function () {
-        //console.log(scope.notifications)
+        // console.log(scope.notifications)
         // write all the contents of the notifications array to the screen
-        //console.log('just logged notifications')
+        // console.log('just logged notifications')
         for (var i = 0; i < scope.notifications.length; i++) {
          // element.append('<p class = "notification">' + scope.notifications[i] + '</p>')
           // ngNotify.set(scope.notifications[i])
           element.append('<div class="alert alert-warning alert-dismissible" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + scope.notifications[i] + '</div>')
-          //console.log(scope.notifications[i])
+          // console.log(scope.notifications[i])
         }
+        scope.$apply()
         // empty notificatiosn
         scope.notifications = []
       }, true)
